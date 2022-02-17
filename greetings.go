6de0d6098c4c59668f6de311d9c7d9ee5e2d2820
tyler-hagen <tylerhagen6@gmail.com>
2022-02-16 23:23:08 -0600

    random greeting format

--- greetings.go
+++ greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
     "fmt"
     "errors"
+    "math/rand"
+    "time"
 )
 
 // Hello returns a greeting for the named person.
@@ -13,6 +15,20 @@ func Hello(name string) (string, error) {
     }
 
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
+}
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    return formats[rand.Intn(len(formats))]
 }
